200040829224629f0a46f4d6dd193a74ab1944b5
Peter <pandreoli@ryerson.ca>
2020-06-21 18:04:34 -0400

    Added basic mod structure.

---
+++ CosmerePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using static Terraria.ModLoader.ModContent;
+
+namespace Cosmere
+{
+	class CosmerePlayer : ModPlayer
+	{
+	}
+}
---
+++ CosmereWorld.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+using static Terraria.ModLoader.ModContent;
+
+namespace Cosmere
+{
+	class CosmereWorld : ModWorld
+	{
+	}
+}
