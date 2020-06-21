5ad05b8805e08ede0f2e9575de40bc308c122457
Peter <pandreoli@ryerson.ca>
2020-06-21 18:35:01 -0400

    Added Buff class for Patternblade

---
+++ Buffs/PatternbladeBuff.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Cosmere.Buffs
+{
+	class PatternbladeBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Patternblade");
+			Description.SetDefault("Adds chance to dodge attacks");
+			//Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+			canBeCleared = false;
+
+		}
+
+		//public override void U
+	}
+}
